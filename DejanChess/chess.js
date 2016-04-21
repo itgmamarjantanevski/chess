9841e83d545148e98f7764cd1aa5b1937f4b5a62
Dejan Keroski <dejan.keroski@ITGMA-CORP>
2016-04-21 11:20:49 +0200

    play ko budala

--- DejanChess/chess.js
+++ DejanChess/chess.js
@@ -17,76 +17,83 @@ function Chessboard() {
 }
 
 //table draw html
-Chessboard.prototype.draw = function() {
-var table = document.createElement('table');
-for (var i = 8 ; i > 0; i--) {
-     var tr = document.createElement('tr');
-     tr.id = i;
-     for (var j = 0; j < 8; j++) {
-         var td = document.createElement('td');
-         var span = document.createElement('span');
-          span.id = String.fromCharCode(j+65) + i;
-         if(i%2 === 0){
-            if(j%2 === 0) {
-                td.className = "white";
-            }else {
-                td.className = "black";
-               
-            }
-            }else {
-             if(j%2 === 0) {
-                td.className = "black";
-             }else {
-                td.className = "white";
-            }
-         }
-         tr.appendChild(td);
-         td.appendChild(span);
-     }
-     table.appendChild(tr);
-}
-document.getElementById("chessBoard").appendChild(table);
-}
-// end of draw
+function drawTable(chessboard) {
+	var div = document.getElementById("chessBoard");
+	div.innerHTML = "";
+	var table = document.createElement('table');
+	for (var i = 8; i > 0; i--) {
+		var tr = document.createElement('tr');
+		tr.id = i;
+		for (var j = 0; j < 8; j++) {
+			var td = document.createElement('td');
+			//console.log(chessboard.at(i-1,j));
+			var span = document.createElement('span');
+			if (chessboard.at(i - 1, j) != null) {
+				span.innerHTML = chessboard.at(i - 1, j).img;
+			} else {
+				span.innerHTML = "";
+			}
 
-Chessboard.prototype.reset = function() {
+			span.id = String.fromCharCode(j + 65) + i;
+
+			if (i % 2 === 0) {
+				if (j % 2 === 0) {
+					td.className = "white";
+				} else {
+					td.className = "black";
+				}
+            } else {
+				if (j % 2 === 0) {
+					td.className = "black";
+				} else {
+					td.className = "white";
+				}
+			}
+			tr.appendChild(td);
+			td.appendChild(span);
+		}
+		table.appendChild(tr);
+	}
+	document.getElementById("chessBoard").appendChild(table);
+}
+//end of draw
+Chessboard.prototype.reset = function () {
 	var i;
-	
 	this.figures = [];
 	this.board = [];
-	for(i=0; i<8; i++) this.board[i] = [null, null, null, null, null, null, null, null];
-	
-	this.set(0, 0, new Rook(WHITE, 1));
-	this.set(0, 1, new Knight(WHITE, 1));
-	this.set(0, 2, new Laufer(WHITE, 1));
-	this.set(0, 3, new Queen(WHITE));
-	this.set(0, 4, new King(WHITE));
-	this.set(0, 5, new Laufer(WHITE, 2));
-	this.set(0, 6, new Knight(WHITE, 2));
-	this.set(0, 7, new Rook(WHITE, 2));
-	
-	for(i=0; i<8; i++) {
-		this.set(1, i, new Pawn(WHITE, i+1));
-		this.set(6, i, new Pawn(BLACK, i+1));
+	for (i = 0; i < 8; i++) this.board[i] = [null, null, null, null, null, null, null, null];
+
+	this.set(0, 0, new Rook(WHITE, "&#9814;", 1));
+	this.set(0, 1, new Knight(WHITE, "&#9816;", 1));
+	this.set(0, 2, new Laufer(WHITE, "&#9815;", 1));
+	this.set(0, 3, new Queen(WHITE, "&#9813;"));
+	this.set(0, 4, new King(WHITE, "&#9812;"));
+	this.set(0, 5, new Laufer(WHITE, "&#9815;", 2));
+	this.set(0, 6, new Knight(WHITE, "&#9816;", 2));
+	this.set(0, 7, new Rook(WHITE, "&#9814;", 2));
+
+	for (i = 0; i < 8; i++) {
+		this.set(1, i, new Pawn(WHITE, "&#9817;", i + 1));
+		this.set(6, i, new Pawn(BLACK, "&#9823;", i + 1));
 	}
-	
-	this.set(7, 0, new Rook(BLACK, 1));
-	this.set(7, 1, new Knight(BLACK, 1));
-	this.set(7, 2, new Laufer(BLACK, 1));
-	this.set(7, 3, new Queen(BLACK));
-	this.set(7, 4, new King(BLACK));
-	this.set(7, 5, new Laufer(BLACK, 2));
-	this.set(7, 6, new Knight(BLACK, 2));
-	this.set(7, 7, new Rook(BLACK, 2));
+
+	this.set(7, 0, new Rook(BLACK, "&#9820;", 1));
+	this.set(7, 1, new Knight(BLACK, "&#9822;", 1));
+	this.set(7, 2, new Laufer(BLACK, "&#9821;", 1));
+	this.set(7, 3, new Queen(BLACK, "&#9819;"));
+	this.set(7, 4, new King(BLACK, "&#9818;"));
+	this.set(7, 5, new Laufer(BLACK, "&#9821;", 2));
+	this.set(7, 6, new Knight(BLACK, "&#9822;", 2));
+	this.set(7, 7, new Rook(BLACK, "&#9820;", 2));
 }
 
-Chessboard.prototype.set = function(row, col, fig , id) {
+Chessboard.prototype.set = function (row, col, fig, id) {
 	if (fig.board === undefined) {
 		Object.defineProperty(fig, 'board', {
-			  value: this,
-			  writable: false,
-			  enumerable: false,
-			  configurable: false
+			value: this,
+			writable: false,
+			enumerable: false,
+			configurable: false
 		});
 		this.figures.push(fig);
 	}
@@ -94,29 +101,29 @@ Chessboard.prototype.set = function(row, col, fig , id) {
 	fig.pos = new Pos(row, col);
 }
 
-Chessboard.prototype.at = function(a, b) {
+Chessboard.prototype.at = function (a, b) {
 	if (a === undefined) return undefined;
 	var pos = a;
-	if (arguments.length == 2) pos = new Pos(a,b);
+	if (arguments.length == 2) pos = new Pos(a, b);
 	if (!pos.isValid()) return undefined;
 	return this.board[pos.row][pos.col];
 }
 
-Chessboard.prototype.moves = function() {
+Chessboard.prototype.moves = function () {
 	var moves = [];
-	this.figures.forEach(function(fig) {
+	this.figures.forEach(function (fig) {
 		var x = fig.moves();
 		if (x && x.length > 0) moves = moves.concat(x);
 	});
 	return moves;
 }
 
-Chessboard.prototype.toString = function() {
+Chessboard.prototype.toString = function () {
 	var s = '';
 	var i, j;
-	for(i=7; i>=0; i--) {
+	for (i = 7; i >= 0; i--) {
 		s += (i + 1) + ' ';
-		for(j=0; j<8; j++) {
+		for (j = 0; j < 8; j++) {
 			s += (this.board[i][j] || '...') + ' ';
 		}
 		s += '\n';
@@ -124,52 +131,73 @@ Chessboard.prototype.toString = function() {
 	s += '   A   B   C   D   E   F   G   H\n';
 	return s;
 }
-//random play 
-Chessboard.prototype.play = function() {
+
+Chessboard.prototype.play = function () {
+	c.turn = true;
     var currentPlayer;
     var chessboard = this;
     var nextMove;
-    var whiteTake=[];
-    var blackTake=[];
+    var whiteTake = [];
+    var blackTake = [];
+
     chessboard.moveCounter = 0;
-   // console.log(" " + c);
-    var allMoves = c.moves();
-    
-    setInterval(function () {
-    console.log("" + c);
-    var whitePlayerMoves = allMoves.filter(function (a) {
-        return a.fig.color.charAt(0) == 'w';}); 
-    var blackPlayerMoves = allMoves.filter(function (b) {
-        return b.fig.color.charAt(0) == 'b';}); 
-    var whiteIndex = Math.floor(Math.random() * whitePlayerMoves.length);
-    var white = whitePlayerMoves[whiteIndex];
-    var blackIndex = Math.floor(Math.random() * blackPlayerMoves.length);
-    var black = blackPlayerMoves[blackIndex];
-    
-         if(c.turn === true) {
-             console.log("white turn" + " " + white.fig + " " + "from" + " " + white.from  + " " + "to" + " "+ white.to);
-             chessboard.board[white.from.col][white.from.row] = null;
-             chessboard.set(white.to.col, white.to.row, white.fig); 
+	// console.log(" " + c);
+	//all mooves
+	var randomPlay = setInterval(function () {
+		var allMoves = c.moves();
+		//console.log("" + c);
+		var whitePlayerMoves = allMoves.filter(function (a) {
+			return a.fig.color.charAt(0) == 'w';
+		});
+		var blackPlayerMoves = allMoves.filter(function (b) {
+			return b.fig.color.charAt(0) == 'b';
+		});
+
+
+		if (c.turn) {
+			var whiteIndex = Math.floor(Math.random() * whitePlayerMoves.length);
+			var white = whitePlayerMoves[whiteIndex];
+			console.log("white turn" + " " + white.fig + " " + "from" + " " + white.from + " " + "to" + " " + white.to);
+			chessboard.board[white.from.col][white.from.row] = null;
+			if (chessboard.board[white.to.col][white.to.row] != null) {
+				//console.log(chessboard.board);
+				var takenFigure = chessboard.board[white.to.col][white.to.row];
+				takenFigure.pos.col = null;
+				takenFigure.pos.row = null;
+				if (takenFigure.type === "KG") {
+					chessboard.set(white.to.col, white.to.row, white.fig);
+					console.log("Game over");
+					return false;
+				}
+			}
+			chessboard.set(white.to.col, white.to.row, white.fig);
         } else {
-             console.log("Black turn" + " " + black.fig + " " + "from"+ " " + black.from + " " + "to" + " " + black.to);
-             chessboard.board[black.from.col][black.from.row]= null;
-             chessboard.set(black.to.col, black.to.row, black.fig); 
+			var blackIndex = Math.floor(Math.random() * blackPlayerMoves.length);
+			var black = blackPlayerMoves[blackIndex];
+			console.log("Black turn" + " " + black.fig + " " + "from" + " " + black.from + " " + "to" + " " + black.to);
+			chessboard.board[black.from.col][black.from.row] = null;
+			if (chessboard.board[black.to.col][black.to.row] != null) {
+				var takenFigure = chessboard.board[black.to.col][black.to.row];
+				takenFigure.pos.col = null;
+				takenFigure.pos.row = null;
+
+				if (takenFigure.type === "KG") {
+					chessboard.set(black.to.row, black.to.col, black.fig);
+					console.log("Game over");
+					alert("Game Over");
+					clearInterval(randomPlay);
+					return false;
+				}
+			}
+			chessboard.`set`(black.to.col, black.to.row, black.fig);
         }
-       if(white.length === 0)  {
-        console.log("Black pieces win");
-        return null;
-       };
-    
-       if(black.length === 0) {
-        console.log("Black pieces win");
-        return null;
-       };
-    c.turn = !c.turn;
-    },1000);
+		c.turn = !c.turn;
+		drawTable(c);
+    }, 1000);
 }
 
-// ===== Common Types ======
 
+// ===== Common Types ======
 function Pos(a, b, c) {
 	if (c === undefined) {
 		this.row = a;
@@ -180,15 +208,15 @@ function Pos(a, b, c) {
 	}
 }
 
-Pos.prototype.off = function(r, c) {
+Pos.prototype.off = function (r, c) {
 	return new Pos(this, r, c);
 }
 
-Pos.prototype.toString = function() {
+Pos.prototype.toString = function () {
 	return LETTERS[this.col] + (this.row + 1);
 }
 
-Pos.prototype.isValid = function() {
+Pos.prototype.isValid = function () {
 	return !(this.row < 0 || this.row > 7 || this.col < 0 || this.col > 7);
 }
 
@@ -198,19 +226,27 @@ function Movement(fig, to) {
 	this.to = to;
 }
 
-Movement.prototype.toString = function() {
+Movement.prototype.toString = function () {
 	return this.fig.toString() + "/" + this.from + "->" + this.to;
 }
 
-function Figure(type, color, index) {
+function Figure(type, color, img, index) {
 	this.type = type;
 	this.color = color;
 	if (index) this.index = index;
+	this.img = img;
 }
 
 
 
-//===== Figure Type ======
+// ===== Figure Type ======
+
+function Figure(type, color, img, index) {
+	this.type = type;
+	this.color = color;
+	if (index) this.index = index;
+	this.img = img;
+}
 
 Figure.parent = function (d) {
 	var b = this;
@@ -219,90 +255,201 @@ Figure.parent = function (d) {
     d.prototype = b === null ? Object.create(b) : (__.prototype = b.prototype, new __());
 }
 
-Figure.prototype.moves = function() {
+Figure.prototype.moves = function () {
 	return [];
-} 
-
-Figure.prototype.toString = function() {
-	return this.color + this.type + (this.index || ''); 
 }
 
-Figure.prototype.init = function() {
+Figure.prototype.moveSteps = function (steps, repeat) {
+	var fig = this;
+    var moves = [];
+    var moveCounter = 0;
+    if (fig.color == BLACK) {
+		steps = steps.map(function (obj) {
+			obj[0] = obj[0] * (-1);
+			obj[1] = obj[1] * (-1);
+			return [obj[0], obj[1]];
+		});
+	}
+
+    for (var i = 0; i < steps.length; i++) {
+		//all steps in pawn
+		var step = steps[i];
+		//current positon
+		var tmp = fig.pos.off(step[0], step[1]);
+
+		var at = this.board.at(tmp);
+
+        while (at === null || at && at.color != fig.color) {
+			if (fig instanceof Pawn) {
+				if (at === null) {
+					if (fig.movePawn(step)) {
+						if (moveCounter < 2) {
+							var pown = fig.pos;
+							if (pown.row == 1) {
+								var tmp2 = fig.pos.off(step[0] + 1, step[1]);
+								moves.push(tmp2);
+							} if (pown.row == 6) {
+								var tmp3 = fig.pos.off(step[0] - 1, step[1]);
+								moves.push(tmp3);
+							}
+							moveCounter++;
+						}
+						moves.push(tmp);
+					}
+				} else if (at != null && at.color != fig.color) {
+					if (fig.pawnAttack(step))
+						moves.push(tmp);
+				}
+			}
+			else moves.push(tmp);
+			if (at != null || !repeat) break;
+			var tmp = tmp.off(step[0], step[1]);
+			var at = this.board.at(tmp);
+        }
+    }
+
+    moves = moves.map(function (x) {
+        return new Movement(fig, x);
+    });
+	return moves;
+}
+Figure.prototype.movePawn = function (tmp) {
+	if ((tmp[0] == 1 && tmp[1] == 0) || (tmp[0] == -1 && tmp[1] == 0)) {
+		return true;
+	} else return false;
+}
+Figure.prototype.pawnAttack = function (tmp) {
+	if ((tmp[0] == 1 && tmp[1] == -1) || (tmp[0] == 1 && tmp[1] == 1) || (tmp[0] == -1 && tmp[1] == 1) || (tmp[0] == -1 && tmp[1] == -1)) {
+		return true;
+	} else return false;
+}
+
+Figure.prototype.toString = function () {
+	return this.color + this.type + (this.index || '');
+}
+
+Figure.prototype.init = function () {
 	Figure.apply(this, arguments);
 }
 
-//===== King Type ======
+// ===== King Type ======
 
 Figure.parent(King);
-function King(color, pos) {
-	this.init('KG', color, null, pos);
+function King(color, img, pos) {
+	this.init('KG', color, img, null, pos);
 }
-
-//===== Queen Type ======
+King.prototype.moves = function () {
+	var steps = [
+		[+1, +0],
+		[+1, +1],
+		[+0, +1],
+		[-1, +1],
+		[-1, +0],
+		[-1, -1],
+		[+0, -1],
+		[+1, -1]
+	];
+	return this.moveSteps(steps, true);
+}
+// ===== Queen Type ======
 
 Figure.parent(Queen);
-function Queen(color, pos) {
-	this.init('QN', color, null, pos);
+function Queen(color, img, pos) {
+	this.init('QN', color, img, null, pos);
 }
 
-//===== Rook Type ======
+
+Queen.prototype.moves = function () {
+	var steps = [
+		[+1, 0],
+		[0, +1],
+		[-1, 0],
+		[0, -1],
+		[+1, +1],
+		[+1, -1],
+		[-1, +1],
+		[-1, -1]
+    ];
+    return this.moveSteps(steps, true);
+}
+// ===== Rook Type ======
 
 Figure.parent(Rook);
-function Rook(color, index, pos) {
-	this.init('R', color, index, pos);
+function Rook(color, img, index, pos) {
+	this.init('R', color, img, index, pos);
 }
 
-//===== Knight Type ======
+Rook.prototype.moves = function (steps) {
+    var steps = [
+		[+1, 0],
+		[0, +1],
+		[-1, 0],
+		[0, -1]
+    ];
+    return this.moveSteps(steps, true);
+}
+
+
+// ===== Knight Type ======
 
 Figure.parent(Knight);
-function Knight(color, index, pos) {
-	this.init('S', color, index, pos);
+function Knight(color, img, index, pos) {
+	this.init('S', color, img, index, pos);
 }
 
-Knight.prototype.moves = function() {
-	var fig = this;
-	var board = this.board;
-	var pos = this.pos;
-	var moves = [
-		pos.off(+2, +1),
-		pos.off(+2, -1),
-		pos.off(-2, +1),
-		pos.off(-2, -1),
-		pos.off(+1, +2),
-		pos.off(+1, -2),
-		pos.off(-1, +2),
-		pos.off(-1, -2)
+Knight.prototype.moves = function () {
+	var steps = [
+		[+2, +1],
+		[+2, -1],
+		[-2, +1],
+		[-2, -1],
+		[+1, +2],
+		[+1, -2],
+		[-1, +2],
+		[-1, -2]
 	];
-	
-	moves = moves.filter(function(x) {
-	    var at = board.at(x);
-	    if (at === undefined) return false;
-	    return (at == null || at && at.color != fig.color);
-	}).map(function(x) {
-		return new Movement(fig, x);
-	});
-		
-	return moves;
+	return this.moveSteps(steps, false);
 }
 
-//===== Laufer Type ======
+// ===== Laufer Type ======
 
 Figure.parent(Laufer);
-function Laufer(color, index, pos) {
-	this.init('L', color, index, pos);
+function Laufer(color, img, index, pos) {
+	this.init('L', color, img, index, pos);
 }
 
-//===== Pawn Type ======
+
+Laufer.prototype.moves = function () {
+	var steps = [
+		[+1, +1],
+		[+1, -1],
+		[-1, +1],
+		[-1, -1]
+    ];
+    return this.moveSteps(steps, true);
+}
+
+
+// ===== Pawn Type ======
 
 Figure.parent(Pawn);
-function Pawn(color, index, pos) {
-	this.init('P', color, index, pos);
+function Pawn(color, img, index, pos) {
+	this.init('P', color, img, index, pos);
 }
 
-//===== Demo code ======
+Pawn.prototype.moves = function () {
+	var steps = [
+		[+1, 0],
+		[+1, -1],
+		[+1, +1]
+	];
+	return this.moveSteps(steps, false);
 
+}
+// ===== Demo code ======
 var c = new Chessboard();
-c.draw();
+drawTable(c)
 c.play();
-// console.log("" + c);
-// console.log("" + c.moves().join(", "));
+
+
+
